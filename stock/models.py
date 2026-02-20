31d81f9a28f82b4766f62ce2ba167ecdaefa161a
KOMBATEYendoubeGloria <kombategloria4@gmail.com>
2026-02-20 11:17:29 +0000

    amelioration de quelques pages avant amelioration final

--- stock/models.py
+++ stock/models.py
@@ -7,7 +7,7 @@ from production.models import Legumineuse
 class Stock(models.Model):
     legumineuse = models.ForeignKey(Legumineuse, on_delete=models.CASCADE)
     quantite_disponible = models.FloatField(help_text="Quantité en kg")
-    date_mise_a_jour = models.DateField(auto_now=True)
+    date_mise_a_jour = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.legumineuse.nom} - {self.quantite_disponible} kg"
